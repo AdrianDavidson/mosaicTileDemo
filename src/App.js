de6ee78bbf49dcb8fb4ad74510b4892a4481ca98
Adrian Davidson <31959515+AdrianDavidson@users.noreply.github.com>
2022-12-14 21:55:17 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Gallery from "./component/Gallery";
+import "./App.scss";
+
+function App() {
+  return <Gallery></Gallery>;
+}
+
+export default App;
---
+++ src/component/Gallery.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { PostCssCalcOptions } from "postcss-calc";
+
+const Gallery = () => {
+  // function Gallery() {
+  const [galleryResults, setGalleryResults] = useState([]);
+
+  //   useEffect(async ()=>{
+  //     // on page load stuff goes in ere
+  //     const headers = {
+  //       Authorization: "563492ad6f91700001000001af2ce6b9e9604a0d9bb5a673cafa3764"
+  //     };
+  //     const apiResponse = await fetch(
+  //       "https://api.pexels.com/v1/search?query=people",
+  //       { headers }
+  //     );
+  //     const apiResponseJSON = await apiResponse.json();
+  //     const photos = apiResponseJSON.photos;
+  //     console.log(photos);
+  //     setGalleryResults(...photos);
+  //   }, []);
+
+  useEffect(() => {
+    const headers = {
+      Authorization: "563492ad6f91700001000001af2ce6b9e9604a0d9bb5a673cafa3764"
+    };
+    const test = fetch("https://api.pexels.com/v1/search?query=technology", {
+      headers
+    })
+      .then(response => response.json())
+      .then(data => setGalleryResults([...data.photos]));
+  }, []);
+
+  return (
+    console.log(galleryResults),
+    (
+      <>
+        <h1>Gallery</h1>
+        <div class="wrapper">
+        <div class="gallery-container">
+          <ul class="masonry-gallery">
+              {/* slice(0, 7) */}
+            {galleryResults.map(post => {
+              return (
+                <li>
+                  <img src={post.src.medium} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        </div>
+      </>
+    )
+  );
+};
+
+export default Gallery;

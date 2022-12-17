61b10608635d92a261dc82e1519e5f5a8c946170
adrian <adrian.davidsonweb@gmail.com>
2022-12-17 14:54:59 +0000

    WIP

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -4,6 +4,36 @@ import { PostCssCalcOptions } from "postcss-calc";
 const Gallery = () => {
   // function Gallery() {
   const [galleryResults, setGalleryResults] = useState([]);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = (event, index) => {
+    console.log(event, index);
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = (event, index) => {
+    setIsHovering(false);
+  };
+
+  // const [hover, setHover] = useState({})
+
+  // const mouseOver = (event, index) => {
+  // setHover(c => {
+  //     return {
+  //         ...c,
+  //         [index]: true
+  //     };
+  // })
+  // }
+
+  // const mouseOut = (event, index) => {
+  //     setHover(c => {
+  //         return {
+  //             ...c,
+  //             [index]: false
+  //         };
+  //     })
+  // }
 
   //   useEffect(async ()=>{
   //     // on page load stuff goes in ere
@@ -35,20 +65,49 @@ const Gallery = () => {
     console.log(galleryResults),
     (
       <>
-        <h1>Gallery</h1>
         <div class="wrapper">
-        <div class="gallery-container">
-          <ul class="masonry-gallery">
+          <div class="gallery-container">
+            <ul class="masonry-gallery">
               {/* slice(0, 7) */}
-            {galleryResults.slice(0, 7).map(post => {
-              return (
-                <li>
-                  <img src={post.src.medium} />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+
+              {
+                galleryResults.map((post, index) => (
+                  <li key={post.id} >
+                    <img key={index} onMouseEnter={(e) => {
+                      handleMouseOver(e, index);
+                    }}
+                      onMouseLeave={(e) => {
+                        handleMouseOut(e, index);
+                      }} src={post.src.medium} />
+                    {isHovering && (
+                      <div key={post.id}>
+                        <h2 id={index + 1}>{post.id}</h2>
+                      </div>
+                    )}
+                  </li>
+
+                  // <div 
+                  //     key={index} 
+                  // onMouseEnter={(e) => {
+                  //     mouseOver(e, index);
+                  // }} 
+                  // onMouseLeave={(e) => {
+                  //     mouseOut(e, index);
+                  // }} 
+                  //     className="flex gap-3"
+                  // >
+                  //     <div>
+                  //         <h1>here</h1>
+                  //     </div>
+                  //     <div hidden={hover[index]}>
+                  //         {post.photographer}
+                  //     </div>
+                  // </div>
+                ))
+              }
+
+            </ul>
+          </div>
         </div>
       </>
     )
